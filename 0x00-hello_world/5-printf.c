bae29ba2232a0093ecd163cc620116c189d236a2
kidistbezabih <kidistbezabh@gmail.com>
2022-03-10 23:04:09 +0300

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - prints "Programming is like building a multilingual puzzle"
+* main - prints some string
 * Return: 0
 */
 int main(void)
 {
-printf("\"Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
 }
