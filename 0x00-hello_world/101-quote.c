835c58919976c9f9c9b4259d46406d607a032725
kidistbezabih <kidistbezabh@gmail.com>
2022-03-10 23:21:30 +0300

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - prints "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main()
+{
+getc("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
