32e31883bf6176081c5337650d9b057053bd5fa6
kidistbezabih <kidistbezabh@gmail.com>
2022-03-11 22:20:00 +0300

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main- description
+*Return:0
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+
+putchar('\n');
+return (0);
+}
