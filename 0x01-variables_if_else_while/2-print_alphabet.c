2d9a663dfc8a68f0ed273650387af6f46cadd8c3
kidistbezabih <kidistbezabh@gmail.com>
2022-03-11 21:52:21 +0300

    small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main- description
+*Return:0
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+printf("%c", ch);
+}
+print("\n");
+return (0);
+}
