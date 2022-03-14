a617a7b744f093a2f9987b6301cd570af9d26cd4
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2022-03-14 12:20:58 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
 *main- description
 *Return:0
 */
-int main(void)
+int main()
 {
-
-return (0);
-
+int i, j, k;
+for (i = 0; i <= 7; i++)
+{
+for (j = i+1; j <= 8; j++)
+{
+for (k = j+1; k <= 9; k++)
+{
+printf("%d%d%d\n", i, j, k)
+}
+}
 }
