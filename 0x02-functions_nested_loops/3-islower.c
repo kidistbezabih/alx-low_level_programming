13f2e6c6ea186e08b3d9465440b1e34955800ba7
kidistbezabih <kidistbezabh@gmail.com>
2022-03-16 00:17:10 +0300

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,13 +8,3 @@ int _islower(int c)
 {
 	return (c >= 97 && c <= 122);
 }
-#include "holberton.h"
-/**
- * _islower - returns 1 if c is lowercase, 0 otherwise
- *  integer to be tested
- * Return: 1 or 0
- */
-int _islower(int c)
-{
-	return (c >= 97 && c <= 122);
-}
