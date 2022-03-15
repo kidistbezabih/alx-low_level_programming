21262cb9708240156c7a7bcef5cf99e835044a04
kidistbezabih <kidistbezabh@gmail.com>
2022-03-16 00:47:08 +0300

    3. islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* _putchar.c */
-int _putchar(char);
-
-/* 1-alphabet.c */
-void print_alphabet(void);
-
-/* 2-print_alphabet_x10.c */
-void print_alphabet_x10(void);
 
 /* 3-islower.c */
 int _islower(int);
