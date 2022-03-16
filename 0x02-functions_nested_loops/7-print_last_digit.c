7dd17c93d20b781ae3735251d0b735b4d2a5ba44
kidistbezabih <kidistbezabh@gmail.com>
2022-03-16 23:54:42 +0300

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * print_last_digit - the function
- *@c: parameter
- * Return: Always c.
- */
+*print_last_digit
+* print_last_digit - the function
+*@c: parameter
+* Return: Always c.
+*/
 int print_last_digit(int c)
 {
 while ((c % 10) > 10)
