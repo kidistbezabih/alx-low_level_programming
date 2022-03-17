ab5b0d4d36126678c15c85ed32410a714eaa1d6d
kidistbezabih <kidistbezabh@gmail.com>
2022-03-17 13:47:53 +0300

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_isupper-function
+*@c: int
+*@x: is int 
+*Return:int 
+*/
+
+int _isupper(int c)
+{
+int x = _putchar();
+if ((x >= 65) && (x <= 90))
+{
+c = 1;
+}
+else
+{
+c = 0;
+}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*  isupper  */
+int _isupper(int c);
+
+#endif
