6c0fe19569a3027cd99a0121273c5c7829fb9577
kidistbezabih <kidistbezabh@gmail.com>
2022-03-17 22:12:23 +0300

    print line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,8 +4,18 @@
 *print_most_numbers-function
 */
 
-void print_most_numbers(void)
+vvoid print_line(int n)
 {
+char p*;
+int i;
+int j;
 
-p[] = 01234567891011121314;
-for int
+p = "01234567891011121314";
+for (j = 0; j < 10; j++)
+{
+for (i = 0; p[i] != '\0'; i++)
+{
+_putchar(p[i]);
+}
+_putchar('\n');
+}
