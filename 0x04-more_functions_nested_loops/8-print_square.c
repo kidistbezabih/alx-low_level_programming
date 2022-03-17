834852b26da981e504b9fcfffa3f5d559e42dbef
kidistbezabih <kidistbezabh@gmail.com>
2022-03-17 23:16:30 +0300

    print diagonal

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_diagonal-function
+*@n: diagonal
+*/
+
+void print_square(int size)
+{
+int i, j;
+
+for (i= 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+}
+}
