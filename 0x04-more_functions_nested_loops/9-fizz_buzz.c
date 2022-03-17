41748eb63bb483e3826dddc00ce266ecfcbdf19a
kidistbezabih <kidistbezabh@gmail.com>
2022-03-17 23:45:16 +0300

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-int i; j; 
+int i, j; 
 i = 0;
 
 char f[] = "Fizz";
@@ -14,7 +14,7 @@ char b[] = "Buzz";
 char fb[] = "FizzBuzz";
 while (i < 100)
 {
-if (((i + 1) % 3) == 0 )
+if (((i + 1) % 3) == 0)
 {
 for (j = 0; f[j] != '\0'; j++)
 {
@@ -23,7 +23,7 @@ _putchar(f[j]);
 }
 
 
-else if (((i + 1) % 5) == 0 )
+else if (((i + 1) % 5) == 0)
 {
 for (j = 0; b[j] != '\0'; j++)
 {
@@ -32,7 +32,7 @@ _putchar(b[j]);
 }
 
 {
-else if((((i + 1) % 3) == 0 ) && (((i + 1) % 5) == 0 ))
+else if((((i + 1) % 3) == 0) && (((i + 1) % 5) == 0))
 {
 for (j = 0; fb[j] != '\0'; j++)
 {
