2fca5e3db1426043aced4372e3afcce905dbc4c0
kidistbezabih <kidistbezabh@gmail.com>
2022-03-18 23:14:13 +0300

    converts an integer into a string 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - converts an integer into a string
+ * @n: integer passed from main
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+unsigned int m;
+
+if (n > 9)
+{
+print_number(n / 10);
+_putchar('0' + (n % 10));
+}
+else if (n > -1)
+{
+_putchar('0' + n);
+}
+else if (n > -10)
+{
+_putchar('-');
+n = n * (-1);
+_putchar('0' + n);
+}
+else
+{
+_putchar('-');
+n = n * (-1);
+m = (unsigned int) n;
+print_number(m / 10);
+_putchar('0' + (m % 10));
+}
+}
