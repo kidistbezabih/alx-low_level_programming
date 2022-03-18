fa6f38e0432de0753d0d9812a005a24823010358
kidistbezabih <kidistbezabh@gmail.com>
2022-03-18 22:50:04 +0300

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,6 +10,8 @@ void print_triangle(int size)
 {
 int i, j;
 int k;
+if (size > 0)
+{
 for (i = 0; i < size; i++)
 {
 for (j = size - i; j > 1; j--)
@@ -22,6 +24,12 @@ for (k = 0; k <= i; k++)
 {
 printf("%c", '#');
 }
+printf("%c", '\n');
+}
+}
+
+else
+{
 printf("%c", '\n');
 }
 }
