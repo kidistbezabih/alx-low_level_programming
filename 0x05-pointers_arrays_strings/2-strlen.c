49749bf28c2c3c913ca13c167353ca560034a0c0
kidistbezabih <kidistbezabh@gmail.com>
2022-03-22 00:47:47 +0300

    printing string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strlen-function
+*@s: char targument
+*Return: c
+*/
+
+int _strlen(char *s)
+{
+int i;
+int c;
+for (i = 0; s[i] != '\0'; i++)
+{
+c = c + 1;
+}
+return (c);
+}
