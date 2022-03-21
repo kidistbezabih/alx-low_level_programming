5c92a90ffc4ed3f0dee82671fc9177d1c09a35d6
kidistbezabih <kidistbezabh@gmail.com>
2022-03-22 02:01:48 +0300

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,20 +6,23 @@
  */
 int _strlen(char *s)
 {
-int i = 0;
+int i;
+i = 0;
 while(s[i])
 i++;
 return (i);
 }
 void rev_string(char *s)
 {
-int len = _strlen(s);
+int len;
+len = _strlen(s);
 char temp;
 int i;
 for(i =0; i< len/2; i++)
 {
 temp = s[i];
-char *_strcpy(char *dest, char *src);s[i] = s[len-1-i];
+char *_strcpy(char *dest, char *src);
+s[i] = s[len-1-i];
 s[len-1-i] = temp;
 }
 }
