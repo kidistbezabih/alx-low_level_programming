4afed51cd7ee78a569abd27e73f03ce3662c5cb3
kidistbezabih <kidistbezabh@gmail.com>
2022-03-22 01:22:20 +0300

    put char

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
-*_strlen-function
+*_puts-function
 *@str: char argument
 *Return: 
 */
 
 void _puts(char *str)
 {
-    int i;
-    char c[];
-    for(i = 0; str[i] != '\0'; i++)
-    {
-    c = c +  
-    }
+int i;
+char c[];
+for(i = 0; str[i] != '\0'; i++)
+{
+c = c + str[i];
+}
+_putchar(c);
 }
