dfea91a8649b53225c858f76b4681c1743499b5a
kidistbezabih <kidistbezabh@gmail.com>
2022-03-22 00:28:05 +0300

    swapping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*reset_to_98-function
+*@n: int argument
+*Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
