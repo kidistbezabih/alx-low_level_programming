9d22a2d5a29489f0c7aed5123663a0b9bf64f03b
kidistbezabih <kidistbezabh@gmail.com>
2022-03-22 01:40:49 +0300

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*peint_rev-function
+*@s: char argument
+*Return: void
+*/
+
+void print_rev(char *s)
+{
+int len;
+int i;
+len = _strlen(char *s);
+
+for(i = len; i > 0; i--)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
