3e9151864c9b37bfef6f8ea3df46850b07c93589
kidistbezabih <kidistbezabh@gmail.com>
2022-03-25 02:51:47 +0300

    strncat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,19 @@
 #include "main.h"
+
+/**
+*_strlen-function
+*@s: argument
+*Return: int
+*/
+
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i])
+i++;
+return (i);
+}
 /**
 *_strcat-function
 *@dest: argument
@@ -6,10 +21,11 @@
 *Return: character
 */
 
-char *_strcat(char* dest, char* src)
+char *_strcat(char *dest, char *src)
 {
-char* ptr = dest + strlen(dest);
-while (*src != '\0') {
+char *ptr = dest + _strlen(dest);
+while (*src != '\0')
+{
 *ptr++ = *src++;
 }
 *ptr = '\0';
