3d662f46fa2f0e2a40622fb9e474c6772145e38f
kidistbezabih <kidistbezabh@gmail.com>
2022-03-24 21:53:07 +0300

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include <main.h>
+/**
+*swap_int-function
+*@dest: argument
+*@src: argument
+*@n: argument
+*Return: character
+*/
+
+char *_strcat(char *dest, char *src);
+{
+int i, j;
+i = 0;
+j = 0;
+while (i ! '\0')
+{
+putchar(dest);
+i++;
+}
+while (j ! '\0')
+{
+putchar(src);
+j++
+}
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strncat(char *dest, char *src, int n);
+
+
+
+#endif
