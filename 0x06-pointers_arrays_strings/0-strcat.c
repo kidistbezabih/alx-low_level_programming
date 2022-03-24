329c60d3d4d5ce0ffb597b00bccb02e28936c053
kidistbezabih <kidistbezabh@gmail.com>
2022-03-24 23:04:33 +0300

    first commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,18 @@
-#include <main.h>
+#include "main.h"
 /**
 *_strcat-function
 *@dest: argument
 *@src: argument
 *Return: character
 */
+
+
 char *_strcat(char *dest, char *src)
 {
-int i, j;
-i = 0;
-j = 0;
-while (i != '\0')
+while (*src != '\0')
 {
-_putchar(*dest);
-i++;
+*dest  += *src;
+src++;
 }
-while (j != '\0')
-{
-_putchar(*src);
-j++;
-}
-_putchar(\n);
+return (dest);
 }
