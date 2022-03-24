ee7306845365712c07674656add70c446eab6841
kidistbezabih <kidistbezabh@gmail.com>
2022-03-25 01:58:59 +0300

    strncat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,14 +5,23 @@
 *@src: argument
 *Return: character
 */
-
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i])
+i++;
+return (i);
+}
 
 char *_strcat(char *dest, char *src)
 {
+
 while (*src != '\0')
 {
 *dest  += *src;
 src++;
+
 }
 return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,7 +7,23 @@
 *Return: character
 */
 
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i])
+i++;
+return (i);
+}
+
 char *_strncat(char *dest, char *src, int n)
 {
+int destlen = _strlen(dest);
+int i;
 
+for (i =0; i < n; && src[i] != '\0'; i++)
+dest[destlen +i] = src[i];
+
+dest[destlen + 1] = '\0';
+return (dest); 
 }
