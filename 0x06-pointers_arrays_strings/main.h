a5badb0cb5f1c45e3d53982e2b822bf1bbadbb99
kidistbezabih <kidistbezabh@gmail.com>
2022-03-25 19:33:16 +0300

    strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,11 +3,15 @@
 
 int _strlen(char *s);
 
-/* descriprion*/
+/* 0 */
 char *_strcat(char *dest, char *src);
-/* description */
+
+/* 1 */
 char *_strncat(char *dest, char *src, int n);
 
-
+/* 2 */
 char *_strncpy(char *dest, char *src, int n);
+
+/* 3 */
+int _strcmp(char *s1, char *s2);
 #endif
