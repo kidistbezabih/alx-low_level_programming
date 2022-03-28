ad26e5088ba196be36403966e0ba88fa28d1d8df
kidistbezabih <kidistbezabh@gmail.com>
2022-03-28 22:00:21 +0300

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_memcpy- returns the length of a string
+ *@dest: char
+ *@n: int
+ *@src: char
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *d = dest;
+char *s = src;
+
+if ((d != NULL) && (c != NULL))
+while (n)
+{
+*d++ = *s++;
+n--;
+}
+return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,4 +3,6 @@
 /* 0 */
 char *_memset(char *s, char b, unsigned int n);
 
+/* 1 */
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
