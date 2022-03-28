5811619b35ef320b8829707e39671fdc53d25a2b
kidistbezabih <kidistbezabh@gmail.com>
2022-03-29 01:36:55 +0300

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * print_chessboard - searches a string for any of a set of bytes
- * @a: array row
- * Return: void
+ * print_chessboard - prints the chessboard
+ * @a: pointer to chessboard
  */
-
 void print_chessboard(char (*a)[8])
 {
-int i, j;
-for (i = 0; i < 8; i++)
-{
-for (j = 0; j < 8; j++)
-{
-_putchar(a[i][j]);
-}
-}
-_putchar('\n');
+	int i, j;
+
+	for (j = 0; j < 8; j++)
+	{
+		for (i = 0; i < 8; i++)
+			_putchar(a[j][i]);
+		_putchar('\n');
+	}
 }
