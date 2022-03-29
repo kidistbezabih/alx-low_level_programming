aeaeb4b7fcba70cf13dc15b859ba707116c54e27
kidistbezabih <kidistbezabh@gmail.com>
2022-03-29 23:41:42 +0300

    diagonal

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,27 @@
 #include "main.h"
 #include <stdio.h>
-#include <math.h>
 
 /**
- * print_diagsums - prints the chessboard
+ * print_diagsums- prints the chessboard
  * @a: pointer to chessboard
  * @size: pointer to chessboard
  */
 
 void print_diagsums(int *a, int size)
 {
+int *p = a;
 int sum1, sum2, i, j;
 sum1 = 0;
 sum2 = 0;
 
-for (i = 0; i < size; i++)
-
+for (i = 0; i < size * size; i += (size + 1))
 {
-sum1 += a[i][i];
+sum1 += *(p + i);
+}
 
-sum2 += a[i][abs(size - i)];
+for (i = size - 1; i < (size * size) - 1; i += (size - 1))
+{
+sum2 += *(p + i);
 }
 printf("%d, %d\n", sum1, sum2);
 }
