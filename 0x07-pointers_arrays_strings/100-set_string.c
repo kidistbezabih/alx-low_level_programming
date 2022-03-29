8c9b51c28eece5cf25e4a5fe707327c816483704
kidistbezabih <kidistbezabh@gmail.com>
2022-03-30 00:46:24 +0300

    set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * set_string - prints the sum of the two diagonals
- * @s: array to be printed
- * @to: size of a
- * Return: void
- */
+* set_string - prints the sum of the two diagonals
+* @s: array to be printed
+* @to: size of a
+* Return: void
+*/
 
 void set_string(char **s, char *to)
 {
