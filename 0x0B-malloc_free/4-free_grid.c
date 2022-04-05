4527734ff8e2d2be8e56e1ec16dd62bc56422655
kidistbezabih <kidistbezabh@gmail.com>
2022-04-05 23:51:23 +0300

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Function
+ * @grid: The 2D array 
+ * @height: The height 
+ *
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+
+i = 0;
+while (i < height)
+{
+free(grid[i]);
+i++;
+}
+free(grid);
+}
