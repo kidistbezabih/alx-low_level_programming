38e811803b8b75a3b2b9b9196ba2360db93cced4
kidistbezabih <kidistbezabh@gmail.com>
2022-04-07 01:25:03 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0;
+
+void)argv;
+while (--argc)
+sum++;
+printf("%i\n", sum);
+return (0);
+}
