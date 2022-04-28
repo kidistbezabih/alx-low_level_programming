a66fe5c860177b47838fa30af69e80bfc1fb6cad
kidistbezabih <kidistbezabh@gmail.com>
2022-04-28 22:13:03 +0300

    to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,14 +3,15 @@
 
 /**
 * print_binary - convert to binary
-* @n:  
+* @n:  argument
+* Return : void
 */
 
 
 void print_binary(unsigned long int n)
 {
-    if (n > 1)
-        print_binary(n / 2);
- 
-    printf("%ld", n % 2);
+if (n > 1)
+print_binary(n / 2);
+
+printf("%ld", n % 2);
 }
