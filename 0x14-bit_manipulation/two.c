9dfebc133af0832f91ae2c2e2a4da96b563ebad7
kidistbezabih <kidistbezabh@gmail.com>
2022-04-28 22:06:24 +0300

    to binary

---
+++ 0x14-bit_manipulation/two.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+#include "main.h"
+
+/**
+* print_binary - convert to binary
+* @n:  
+*/
+
+
+void print_binary(unsigned long int n)
+{
+    if (n > 1)
+        print_binary(n / 2);
+ 
+    printf("%ld", n % 2);
+}
