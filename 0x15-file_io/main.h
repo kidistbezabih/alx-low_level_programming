27cd291453c4a4a663cd1c647e6fb997ef0f349d
kidistbezabih <kidistbezabh@gmail.com>
2022-05-02 22:28:36 +0300

    read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif  /* ENDIF*/
